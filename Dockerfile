ff4ecd74181a810447183af9cf947a5eae3742a7
cheatm <862786917@qq.com>
2017-09-11 10:31:42 +0800

    sa

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ADD routing /routing
 ENV fxdayu /fxdayu
 
 RUN /bin/bash /routing/timezone.sh
+RUN /bin/bash /routing/setrq.sh
 RUN /bin/bash /routing/sinta.sh
 RUN /bin/bash /routing/setcron.sh
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = open("requirements.txt").read().split("\n")
 
 setup(
     name="fxdayu_sinta",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=requires,
     entry_points={"console_scripts": ["sinta = fxdayu_sinta.sinta:sinta",
