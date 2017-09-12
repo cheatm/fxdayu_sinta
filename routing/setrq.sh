0b5f9f255fa08992518654b4d69b768f7f871512
cheatm <862786917@qq.com>
2017-09-12 15:53:05 +0800

    adjust commands

--- routing/setrq.sh
+++ routing/setrq.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-
+# Install compiler
 apt-get install -y gcc
 apt-get install -y g++
-
+# Install rqalpha
 pip install bcolz
 pip install rqalpha
-
+# Generate language pack
+apt-get install -y locales
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
